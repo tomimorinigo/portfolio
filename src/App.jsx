25791de093bb0ee6909e0b8784b49e7b8a430c60
tomiimorinigo <morinigotomas1@gmail.com>
2024-01-08 12:45:57 -0300

    Update meta tags and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <section className="main">
+    <main className="main">
       <h1>Tomas Morinigo</h1>
       <div className='main__center'>
         <h2>Work in progress</h2>
@@ -13,18 +13,18 @@ function App() {
 
         <ul className='social-media'>
           <li><a href="https://github.com/tomimorinigo">
-              <i class="fa-brands fa-github"></i>
+              <i className="fa-brands fa-github"></i>
             </a></li>
           <li><a href="https://www.linkedin.com/in/tomas-morinigo-3508a9204/">
-              <i class="fa-brands fa-linkedin"></i>
+              <i className="fa-brands fa-linkedin"></i>
             </a></li>
           <li><a href="mailto:morinigotomas1@gmail.com">
-              <i class="fa-solid fa-envelope"></i>
+              <i className="fa-solid fa-envelope"></i>
             </a></li>
         </ul>
 
       </div>
-    </section>
+    </main>
   )
 }
 
