f2e628bd650dd507b63936bca18b22815e9d5788
tomiimorinigo <morinigotomas1@gmail.com>
2024-01-17 13:00:38 -0300

    Update logo and header styles, update main info, add social aside

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
     <>
       <header className='header'>
         <div className='logo'>
-          <img src="logo.png" alt="Tomas Morinigo logo" />
+          <a href="/">
+            <img src="logo-3.png" alt="Tomas Morinigo logo" />
+          </a>
         </div>
         <div className='nav'>
           <ul>
@@ -14,34 +16,32 @@ function App() {
             <li><a href="#skills">Skills</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
-            <li><a href="#">Resume</a></li>
+            <li><a href="#" id="resume"><span>Resume</span></a></li>
           </ul>
         </div>
       </header>
+
       <main className="main">
-        <h1>Tomas Morinigo</h1>
-        <div className='main__center'>
-          <h2>Work in progress</h2>
-          <p>Soon my personal portfolio will be available! <br />
-          Thank you for visiting my website
-          </p>
-
-          <ul className='social-media'>
-            <li><a href="https://github.com/tomimorinigo">
-                <i className="fa-brands fa-github"></i>
-              </a></li>
-            <li><a href="https://www.linkedin.com/in/tomas-morinigo-3508a9204/">
-                <i className="fa-brands fa-linkedin"></i>
-              </a></li>
-            <li><a href="mailto:morinigotomas1@gmail.com">
-                <i className="fa-solid fa-envelope"></i>
-              </a></li>
-          </ul>
-
-        </div>
+        <section className='main-section'>
+          <p>Hi, I&apos;m</p>
+          <h1>Tomas Morinigo</h1>
+          <h2>A <span>Full-Stack Web Developer</span> with Java, Spring and React</h2>
+        </section>
       </main>
+
+      <aside className='aside-social'>
+        <a href="https://github.com/tomimorinigo">
+              <i className="fa-brands fa-github"></i>
+          </a>
+        <a href="https://www.linkedin.com/in/tomas-morinigo-3508a9204/">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        <a href="mailto:morinigotomas1@gmail.com">
+            <i className="fa-solid fa-envelope"></i>
+          </a>
+          <hr className="white-line" />
+      </aside>
     </>
-    
   )
 }
 
